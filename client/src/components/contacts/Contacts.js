85ff0e8e25c7181c27c57e1965c006b1f61fedde
IPSIT SAHOO <ipsit.sahoo11@gmail.com>
2020-04-12 21:12:24 +0530

    Adding filter, and other CRUDUXpart to the client UX

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,27 @@
+import React, { useContext, Fragment } from "react";
+import ContactItem from "./ContactItem";
+import ContactContext from "../../context/contact/ContactContext";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem contact={contact} key={contact.id} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem contact={contact} key={contact.id} />
+          ))}
+    </Fragment>
+  );
+};
+
+export default Contacts;
