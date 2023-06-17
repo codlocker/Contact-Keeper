90c635f1cc2e67d0674e5966ca7a897f6e9dc6eb
codlocker <isahoo1@asu.edu>
2023-06-17 05:23:24 -0700

    update

---
+++ client/build/precache-manifest.cdf842f6f654a307e6cf28df70ad8663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83772b446182a7027226",
+    "url": "/static/css/main.a3168cc3.chunk.css"
+  },
+  {
+    "revision": "83772b446182a7027226",
+    "url": "/static/js/main.83772b44.chunk.js"
+  },
+  {
+    "revision": "b7b65f1824ffaf23660f",
+    "url": "/static/js/1.b7b65f18.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08a4e7ec6153a5474bc8c14213fea462",
+    "url": "/index.html"
+  }
+];
